5d490191f51c26f340485b54d8738d0c4cb0493e
rainierwolfcastle <das.bobby2000@gmail.com>
2016-01-30 19:38:27 +0000

    update node

--- node/hello.js
+++ node/hello.js
@@ -5,7 +5,7 @@ const port = 5000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Steev\n');
+  res.end('Hello Steev');
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
